a1ad818e7937ebaa41539b25ea95cc55640f0cda
almogbenlulu <almogbl@moveo.co.il>
2022-05-03 20:54:52 +0300

    Table task
    A table with a button that reset color

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  document.querySelector('.Upper-table thead').classList.add("pink")
+  document.querySelector('.Upper-table thead').style.background = 'black'
+  document.querySelector('.Mid-table thead').style.color = 'white'
+  document.querySelector('.Mid-table thead').style.background = 'black'
+  document.querySelector('.Low-table thead').style.color = 'white'
+  document.querySelector('.Low-table thead').style.background = 'black'
+  }
