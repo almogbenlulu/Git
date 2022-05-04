5b6b86a36c9454157e0cfd8d11c13ee55a446560
almogbenlulu <almogbl@moveo.co.il>
2022-05-04 13:25:28 +0300

    active
    pink

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function changeColor() {
-  document.querySelector('.Upper-table').style.color = 'white'
-  document.querySelector('.Upper-table').style.background = 'black'
-  document.querySelector('.Mid-table').style.color = 'white'
-  document.querySelector('.Mid-table').style.background = 'black'
-  document.querySelector('.Low-table').style.color = 'white'
-  document.querySelector('.Low-table').style.background = 'black'
-
-  // document.querySelector('.Upper-table').classList.remove('Upper-table')
-  // document.querySelector('.Mid-table').classList.remove('Mid-table')
-  // document.querySelector('.Low-table').classList.remove('Low-table')
-  // document.querySelector('table thead').classList.add('pink')
+  document.querySelector('.Upper-table').classList.remove('Upper-table')
+  document.querySelector('.Mid-table').classList.remove('Mid-table')
+  document.querySelector('.Low-table').classList.remove('Low-table')
+  document.querySelector('table thead').classList.add('pink')
   }
