c55943a47991a3e026a533ec2591b57712096218
almogbenlulu <almogbl@moveo.co.il>
2022-05-04 09:15:38 +0300

    Undo evet
    Click the button twice to reverse the color changing

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function changeColor() {
-  document.querySelector('.Upper-table thead').classList.add("pink")
-  document.querySelector('.Upper-table thead').style.background = 'black'
-  document.querySelector('.Mid-table thead').style.color = 'white'
-  document.querySelector('.Mid-table thead').style.background = 'black'
-  document.querySelector('.Low-table thead').style.color = 'white'
-  document.querySelector('.Low-table thead').style.background = 'black'
+  document.querySelector('.Upper-table').style.color = 'white'
+  document.querySelector('.Upper-table').style.background = 'black'
+  document.querySelector('.Mid-table').style.color = 'white'
+  document.querySelector('.Mid-table').style.background = 'black'
+  document.querySelector('.Low-table').style.color = 'white'
+  document.querySelector('.Low-table').style.background = 'black'
+
+  // document.querySelector('.Upper-table').classList.remove('Upper-table')
+  // document.querySelector('.Mid-table').classList.remove('Mid-table')
+  // document.querySelector('.Low-table').classList.remove('Low-table')
+  // document.querySelector('table thead').classList.add('pink')
   }
